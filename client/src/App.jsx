9a9b432ee5fadc90321204a43140e1bbafc95035
Sumon Ahmed <sumonahmedjubayer@gmail.com>
2024-07-24 02:51:37 +0100

    checking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ export default function App() {
       <Route path='/signup' element={<SignUp/>} />
       <Route path='/dashboard' element={<Dashboard/>} />
       <Route path='/projects' element={<Projects/>} />
+      <Route path='/projects' element={<Projects/>} />
     </Routes>
    </BrowserRouter>
   )
